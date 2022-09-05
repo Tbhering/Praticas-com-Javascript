0982f9e3b50530e733a31234e2d8c8c731709181
Thalles Bhering <thbg1@hotmail.com>
2022-09-05 12:47:09 -0300

    Melhorias em codigo/leitura de lista/organização

---
+++ Lista de convidados.js
@@ -0,0 +1,5 @@
+
+const idade = 18;
+const listaDeConvidados = 100;
+
+const nomeInserido = idade >= 18 && listaDeConvidados <=100;
--- aprimoranddo.js
+++ aprimoranddo.js
@@ -2,26 +2,42 @@
 const listaDeDestino = new Array("São paulo", "Rio de Janeiro", "salvador");
 
 
-const idadeComprador = 18;
-const elaEstaacompanhada = false;
-const passagemComprada = false;
-const destino = "Rio de Janeiro";
+const idadeComprador = 17;
+const elaEstaacompanhada = true;
+let passagemComprada = false;
+const destino = "salvador";
+
 
 
 console.log("\n Destinos possíveis:");
 console.log(listaDeDestino);
 
-const podeComprar = idadeComprador >= 18 || elaEstaacompanhada == true;
+//let podeComprar = idadeComprador >= 18 || elaEstaacompanhada == true;
+
 
 let contador = 0;
-while(contador<3){
-    if(listaDeDestino[contador] == destino){
+let destinoExiste = false
+for (let i = 0; i < 3; i++) {
+    if (listaDeDestino[i] == destino);
+    passagemComprada = true;
+    destinoExiste = true;
+  if ( idadeComprador >= 18 || elaEstaacompanhada == true
+
+    ){
         console.log("Destino existe");
-    }else{
-        console.log("Destino não existe")
     };
-    contador += 1;
+    break;
+   
 }
+
+if (idadeComprador >= 18 || elaEstaacompanhada == true && passagemComprada == true && destinoExiste == true) {
+    
+    console.log("Boa-Viagem!")
+} else {
+    console.log("Desculpa, houve algum erro! \n Tente novamente em alguns segundos.")
+}
+
+
 
 
 
